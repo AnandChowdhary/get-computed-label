030d75b5f95c88588e715b18879f1cf997003ae4
Anand Chowdhary <mail@anandchowdhary.com>
2019-09-12 13:50:04 +0530

    :sparkles: Add basic logic

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-export default () => {
-  return ""
+const getComputedLabel = (element: HTMLElement) => {
+  if (element) {
+
+    // The element's `aria-label`
+    const ariaLabel = element.getAttribute("aria-label");
+    if (ariaLabel) return ariaLabel;
+
+    // The value of the element
+    const innerText = element.innerText;
+    if (innerText) return innerText;
+
+    // If it's an image/etc., alternate text
+    // Even if it's an empty alt attribute alt=""
+    if (
+      element.tagName === "APPLET" ||
+      element.tagName === "AREA" ||
+      element.tagName === "IMG" ||
+      element.tagName === "INPUT"
+    ) {
+      const altText = element.getAttribute("alt");
+      if (typeof altText === "string") return altText;
+    }
+  }
 }
+
+export default getComputedLabel;
+
+const elt = document.querySelector("p");
+if (elt) console.log(getComputedLabel(elt));
